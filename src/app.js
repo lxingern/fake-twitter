a0ed19cd15326ac5ab8f23dc552ecbf0d04fe7a9
Xing Ern <lxingern@gmail.com>
2023-01-18 20:40:49 +0800

    create index.ejs, set up route to display on homepage

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
+const path = require('path')
 const express = require('express')
+const ejsMate = require('ejs-mate')
 
 const app = express()
+
+const viewsPath = path.join(__dirname, '../templates/views')
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs')
+app.set('views', viewsPath)
+
+app.get('', (req, res) => {
+    res.render('index')
+})
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000!')
