284b36db594ab2c72ddad0da1adb1d4a9bbae68e
Xing Ern <lxingern@gmail.com>
2023-01-19 13:30:09 +0800

    create route to create a new tweet (post /tweets)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
+require('./db/mongoose')
 const ejsMate = require('ejs-mate')
+const Tweet = require('./models/tweet')
 
 const app = express()
 
@@ -10,8 +12,20 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs')
 app.set('views', viewsPath)
 
+app.use(express.json())
+
 app.get('', (req, res) => {
     res.render('index')
+})
+
+app.post('/tweets', async (req, res) => {
+    const tweet = new Tweet(req.body)
+    try {
+        await tweet.save()
+        res.status(201).send(tweet)
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 app.listen(3000, () => {
